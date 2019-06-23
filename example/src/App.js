ba84f8132c5f68233d04c5c5c5e9a1743cea5cf0
Mandour <ahmedmandur@hotmail.com>
2019-06-23 11:06:35 +0300

    fix the static pages

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-here-autocomplete'
+import AutoCompleteText from "react-here-autocomplete";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{ textAlign: "center", width: "100%" }}>
+        <AutoCompleteText
+          items={["Ahmed", "Amier", "Alaa"]}
+          placeholder="Start Typing"
+          style={{ width: 500 }}
+          appid="IDZ2j4o9IdjMZO42fKwO"
+          appcode="4TSWoPbt3OQLpfqwLA2IdA"
+        />
       </div>
-    )
+    );
   }
 }
